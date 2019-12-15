2e0c89771b06a373eaadeb4f5e8bb750ed07f646
chinjanggg <ice_sripaiboon@hotmail.com>
2019-12-15 21:49:45 +0700

    Update test_accuracy.py

--- python/test_accuracy.py
+++ python/test_accuracy.py
@@ -12,9 +12,9 @@ def test_accuracy(no_of_res, expect_val, test):
             cmpr_row = [t_val[i] for t_val in test if(not (t_val[i]==-1).all())]
             for j, e_val in enumerate(e_row):
                 cmpr_val = [val[j] for val in cmpr_row]
-                accuracy = accuracy + (cmpr_val.count(e_val) / no_of_res)
+                accuracy = accuracy + cmpr_val.count(e_val)
             
         #Make it percent
-        accuracy = accuracy / no_of_val * 100
+        accuracy = (accuracy / (no_of_val * no_of_res)) * 100
         
     return accuracy
